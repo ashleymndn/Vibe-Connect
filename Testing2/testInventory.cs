785c627551efadaa4992f761319017ac5a0484de
nabihakhaled26 <nabihakhaled26@gmail.com>
2026-04-30 16:39:21 +0400

    Added testing for inventory class

---
+++ Testing2/testInventory.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class testInventory
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            Assert.IsNotNull( AnInventory );
+        }
+    }
+}
