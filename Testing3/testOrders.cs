4fd79df87af2719855a726dcfbdada21ba56bd2e
Ashley Mondragon <ashleyreneemondragonx@gmail.com>
2026-04-30 21:27:15 +0400

    Added testing for the orders class

---
+++ Testing3/testOrders.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class testOrders
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
