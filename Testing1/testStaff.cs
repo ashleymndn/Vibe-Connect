558ed6cf5496cc5c3234c65409177da24991df20
Saarah Shaikh <P2879363@my365.dmu.ac.uk>
2026-05-05 18:53:19 +0400

    Add testing for the staff class

---
+++ Testing1/testStaff.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+[TestClass]
+public class testStaff
+{
+    [TestMethod]
+    public void InstanceOK()
+    {
+        clsStaff AStaff = new clsStaff();
+        Assert.IsNotNull(AStaff);
+    }
+}
