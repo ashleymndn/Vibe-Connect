d366183fce8c16c6caacde0aa11476d6898ff331
nabihakhaled26 <nabihakhaled26@gmail.com>
2026-05-07 16:25:25 +0400

    Added presentation layer

--- AdminSystem/2InventoryDataEntry.aspx.cs
+++ AdminSystem/2InventoryDataEntry.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
+
+
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -11,4 +14,66 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void ProductName_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void InventoryId_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ProductId_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ProductPrice_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void QuantityInStock_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void LastUpdated_TextChanged(object sender, EventArgs e)
+    {
+
+      
+    }
+
+
+    protected void StockStatus_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Active_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        clsInventory AnInventory = new clsInventory();
+        AnInventory.InventoryId=Convert.ToInt32(InventoryIdTextBox.Text);
+        AnInventory.ProductId=Convert.ToInt32(ProductIdTextBox.Text);
+        AnInventory.ProductName= ProductNameTextBox.Text;
+        AnInventory.ProductPrice = Convert.ToDecimal(ProductPriceTextBox.Text);
+        AnInventory.QuantityInStock = Convert.ToInt32(QuantityInStockTextBox.Text);
+        AnInventory.LastUpdated = Convert.ToDateTime(LastUpdatedTextBox.Text);
+        AnInventory.StockStatus = StockStatusTextBox.Text;
+        AnInventory.Active = chkActive.Checked;
+
+        Session["AnInventory"] = AnInventory;
+
+        Response.Redirect("2InventoryViewer.aspx");
+    }
+
+
 }
--- AdminSystem/2InventoryViewer.aspx.cs
+++ AdminSystem/2InventoryViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsInventory AnInventory= new clsInventory();
+        AnInventory = (clsInventory)Session["AnInventory"];
+        Response.Write(AnInventory.InventoryId);
+        Response.Write(AnInventory.ProductId);
+        Response.Write(AnInventory.ProductName);
+        Response.Write(AnInventory.ProductPrice);
+        Response.Write(AnInventory.QuantityInStock);
+        Response.Write(AnInventory.LastUpdated);
+        Response.Write(AnInventory.StockStatus);
+        Response.Write(AnInventory.Active);
     }
 }
