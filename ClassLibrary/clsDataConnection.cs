ac8475806777faec01b6a29e682b9ceffa713465
Saarah Shaikh <P2879363@my365.dmu.ac.uk>
2026-05-31 16:56:20 +0400

    Completed Staff Find and validation methods

--- ClassLibrary/clsDataConnection.cs
+++ ClassLibrary/clsDataConnection.cs
@@ -33,9 +33,7 @@ public class clsDataConnection
 
     private string GetConnectionString()
     {
-        System.Net.WebClient client = new System.Net.WebClient();
-        string downloadString = client.DownloadString("http://localhost:5000/");
-        return downloadString;
+        return "Data Source=vibeconnectserver.database.windows.net;Initial Catalog=vibeconnectdb;User ID=vibeconnectadmin;Password=Admin@123;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 
     public string GetDBName()
