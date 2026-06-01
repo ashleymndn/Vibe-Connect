24528f13a0b5f88bc1ea39615bdec881e3f02e5b
Saarah Shaikh <P2879363@my365.dmu.ac.uk>
2026-06-01 19:02:44 +0400

    Completed Staff login page

---
+++ AdminSystem/1StaffLogin.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ClassLibrary;
+
+public partial class _1StaffLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        clsStaffUser AnUser = new clsStaffUser();
+
+        string UserName = txtUserName.Text;
+        string Password = txtPassword.Text;
+
+        Boolean Found = false;
+
+        Found = AnUser.FindUser(UserName, Password);
+
+        if (txtUserName.Text == "")
+        {
+            lblError.Text = "Enter a username";
+        }
+        else if (txtPassword.Text == "")
+        {
+            lblError.Text = "Enter a password";
+        }
+        else if (Found == true)
+        {
+            Response.Redirect("1StaffList.aspx");
+        }
+        else
+        {
+            lblError.Text = "Login details are incorrect. Please try again.";
+        }
+    }
+}
